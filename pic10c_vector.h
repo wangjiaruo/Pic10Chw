a8946c53e15cd3106494b843fb5ac2f0bd18be5c
wangjiaruo <wangjiaruo98@ucla.edu>
2018-04-24 18:13:54 -0700

    sketch

---
+++ pic10c_vector.h
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::sqrt;
+using std::sqrt;
+using std::boolalpha;
+
+namespace Pic10c{
+	class vector{
+
+	
+	}
+}
+
